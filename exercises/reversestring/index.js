88979b313c6112ed1396c94988d917cb4d840b4e
AlexanderShpilka <sandro.bot.evil@gmail.com>
2019-05-07 20:52:22 +0300

    Reverse string solutions

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,36 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+// 4th solution
+function reverse(str) {
+  return Array.from(str).reduce((accum, current) => current + accum, '');
+}
 
 module.exports = reverse;
+
+// 1st solution
+// function reverse(str) {
+//   let result = '';
+
+//   for (let i = str.length - 1; i >= 0; i--) {
+//     result += str[i];
+//   }
+
+//   return result;
+// }
+
+// 2nd solution
+// function reverse(str) {
+//   return str.split('').reverse().join('');
+// }
+
+// 3rd solution
+// function reverse(str) {
+//   let result = '';
+
+//   for (let char of str) {
+//     result = char + result;
+//   }
+
+//   return result;
+// }
