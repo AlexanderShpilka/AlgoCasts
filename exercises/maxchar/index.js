781dbb360c0a1c5d5505c527d35555b2d7e8f4af
AlexanderShpilka <sandro.bot.evil@gmail.com>
2019-05-10 17:46:23 +0300

    Adds maxChar solution

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,26 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  let charToReturn;
+
+  let maxOccurencies = 0;
+  const map = {};
+
+  for (let char of str) {
+    let count = map[char] || 0;
+
+    map[char] = ++count;
+  }
+
+  for (let key in map) {
+    if (map[key] >= maxOccurencies) {
+      charToReturn = key;
+      maxOccurencies = map[key];
+    }
+  }
+
+  return charToReturn;
+}
 
 module.exports = maxChar;
